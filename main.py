071952e0ace9f6411c4cfabce9b9083e6b66f5e4
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-15 14:10:57 -0400

    Added Snake Head Movements

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import pygame
 from pygame.locals import *
 
 window_width, window_height = 800, 600
+block_dimension = 50
 
+def addBlock(image_path):
+    return pygame.transform.scale(pygame.image.load(image_path), (block_dimension, block_dimension))
 
-def addblock(image_path):
-    block_width, block_height = 100, 100
-    return pygame.transform.scale(pygame.image.load(image_path), (block_width, block_height))
+def buildBlock(block, x, y):
+    window.fill((143,188,143))
+    window.blit(block, (x, y))
+    pygame.display.flip()
 
 
 pygame.init()
@@ -16,15 +20,29 @@ window.fill((143,188,143))
 pygame.display.set_caption("Karan's Snake Game")
 pygame.display.set_icon(pygame.image.load('assets/snake_game_logo.png'))
 
-block = addblock("assets/snake_head.png")
-window.blit(block, (0, 0))
+head = addBlock("assets/snake_head.png")
+head_x, head_y = 0, 0
+window.blit(head, (head_x, head_y ))
 
 pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            pass
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                head_y -= block_dimension
+                buildBlock(head, head_x, head_y)
+            elif event.key == K_DOWN:
+                head_y += block_dimension
+                buildBlock(head, head_x, head_y)
+            elif event.key == K_LEFT:
+                head_x -= block_dimension
+                buildBlock(head, head_x, head_y)
+            elif event.key == K_RIGHT:
+                head_x += block_dimension
+                buildBlock(head, head_x, head_y)
 
-        if event.type == pygame.QUIT:
+
+
+        if event.type == QUIT:
             running = False
