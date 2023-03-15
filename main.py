a2c0e3752f86d2a665fa82c7b90f83cabbb50d01
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-15 15:23:03 -0400

    Added Food

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
-
-window_width, window_height = 800, 600
+import random
+window_width, window_height = 500, 500
 block_dimension = 50
 
 class Block:
@@ -10,23 +10,47 @@ class Block:
         self.x = x
         self.y = y
 
+class Food:
+    def __init__(self) -> None:
+        self.x = random.randint(0, 9) * block_dimension
+        self.y = random.randint(0, 9) * block_dimension
+        self.block = Block("assets/rat_food.png", self.x, self.y)
+        
+    def getPosition(self):
+        return self.x, self.y
+food = Food()
+
 class Snake:
     def __init__(self) -> None:
         self.head = Block("assets/snake_head.png", 0, 0)
+        self.body = []
 
-    def updateSnake(self, x, y) -> None:
+    def updateSnakePosition(self, x, y) -> None:
+        if self.body:
+            self.body.insert(0, self.body.pop())
+            self.body[0].x = self.head.x
+            self.body[0].y = self.head.y
         self.head.x += x
         self.head.y += y
+
+    def addSnakeBlock(self) -> None:
+        if self.body:
+            self.body.append(Block("assets/peach_body.png", self.body[-1].x, self.body[-1].y))
+        else:
+            self.body.append(Block("assets/peach_body.png", self.head.x, self.head.y))
 
 
 class SnakeGame:
     def __init__(self) -> None:
+        self.score = 0
         pygame.init()
 
         self.window = pygame.display.set_mode((window_width, window_height))
         self.window.fill((143,188,143))
         pygame.display.set_caption("Karan's Snake Game")
         pygame.display.set_icon(pygame.image.load('assets/snake_game_logo.png'))
+
+        self.food = Food()
 
         self.snake = Snake()
 
@@ -58,9 +82,24 @@ class SnakeGame:
 
     def moveSnake(self, x, y):
         self.window.fill((143,188,143))
-        self.snake.updateSnake(x, y)
-        self.window.blit(self.snake.head.block, (self.snake.head.x, self.snake.head.y))
+        self.snake.updateSnakePosition(x, y)
+        self.buildSnake()
+        print(self.snake.head.x, self.food.block.x, self.snake.head.y, self.food.block.y)
+        if ((self.snake.head.x == self.food.block.x) and (self.snake.head.y == self.food.block.y)):
+            self.food = Food()
+            self.score+=1
+            self.snake.addSnakeBlock()
+            self.buildSnake()
         pygame.display.flip()
+
+    def buildSnake(self):
+        self.window.blit(self.snake.head.block, (self.snake.head.x, self.snake.head.y))
+
+        self.window.blit(self.food.block.block, (self.food.block.x, self.food.block.y))
+
+        for block in self.snake.body:
+            self.window.blit(block.block, (block.x, block.y))
+
 
 
 
