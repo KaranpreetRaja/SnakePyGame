167572815fa833aaf18f7814d867130c8464ce46
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-20 19:55:55 -0400

    Adds footer and kill border and score indicator

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import pygame
 from pygame.locals import *
 import random
 import time
-window_width, window_height = 500, 500
-block_dimension = 50
+window_width, window_height = 500, 520
+padding = 20
+block_dimension = 25
+
+#music
+pygame.mixer.init()
+pygame.mixer.music.load("assets/pygame_music.mp3")
+pygame.mixer.music.play(-1)
+
+class GameBlock:
+    def __init__(self, x, y, width, height, color) -> None:
+        self.x = x
+        self.y = y
+        self.block = pygame.Surface((width, height))
+        self.block.fill(pygame.Color(color))
 
 class Block:
     def __init__(self, image_path, x, y) -> None:
@@ -38,7 +51,7 @@ class Snake:
         if self.body:
             self.body.append(Block("assets/peach_body.png", self.body[-1].x, self.body[-1].y))
         else:
-            self.body.append(Block("assets/peach_body.png", self.head.x, self.head.y))
+            self.body.append(Block("assets/peach_body.png", self.head.x-block_dimension, self.head.y-block_dimension))
 
 
 class SnakeGame:
@@ -50,6 +63,8 @@ class SnakeGame:
         self.window.fill((143,188,143))
         pygame.display.set_caption("Karan's Snake Game")
         pygame.display.set_icon(pygame.image.load('assets/snake_game_logo.png'))
+
+        self.footer = GameBlock(0, window_width, window_height-padding, padding, "White")
 
         self.food = Food()
 
@@ -63,6 +78,20 @@ class SnakeGame:
         self.lastmove = time.time()
         while self.running == True:
             self.gamerun()
+
+        #Score Screen
+        overlay = pygame.Surface((window_width, window_height), pygame.SRCALPHA)
+        overlay.fill((128, 128, 128, 128))
+
+        score_text = pygame.font.SysFont(None, 64).render(f"Score: {self.score}", True, (255, 255, 255))
+
+        self.window.blit(overlay, (0, 0))
+        self.window.blit(score_text, (window_width / 2 - score_text.get_width() / 2, window_height / 2 - score_text.get_height() / 2))
+        pygame.display.update()
+
+        time.sleep(1)
+
+
     
     def gamerun(self):
         for event in pygame.event.get():
@@ -71,20 +100,25 @@ class SnakeGame:
                     self.running  = False
 
                 if event.key == K_UP:
-                    self.moveSnake(0, -block_dimension)
-                    self.direction = 0
+                    if (self.direction != 1):
+                        self.moveSnake(0, -block_dimension)
+                        self.direction = 0
                 elif event.key == K_DOWN:
-                    self.moveSnake(0, block_dimension)
-                    self.direction = 1
+                    if (self.direction != 0):
+                        self.moveSnake(0, block_dimension)
+                        self.direction = 1
                 elif event.key == K_LEFT:
-                    self.moveSnake(-block_dimension, 0)
-                    self.direction = 2
+                    if (self.direction != 3):
+                        self.moveSnake(-block_dimension, 0)
+                        self.direction = 2
                 elif event.key == K_RIGHT:
-                    self.moveSnake(block_dimension, 0)
-                    self.direction = 3
+                    if (self.direction != 2):
+                        self.moveSnake(block_dimension, 0)
+                        self.direction = 3
 
             if event.type == QUIT:
                 self.running  = False
+
         if(time.time()-self.lastmove > 0.25):
             if (self.direction == 0):
                 self.moveSnake(0, -block_dimension)
@@ -94,7 +128,15 @@ class SnakeGame:
                 self.moveSnake(-block_dimension, 0)
             elif (self.direction == 3):
                 self.moveSnake(block_dimension, 0)
+            
             self.lastmove = time.time()
+    
+        if(self.snake.head.x > window_width or self.snake.head.x < 0 or self.snake.head.y > window_height-padding or self.snake.head.y < 0):
+            self.running = False
+
+        for body_block in self.snake.body:
+            if self.snake.head.x == body_block.x and self.snake.head.y == body_block.y:
+                self.running = False
 
 
 
@@ -111,12 +153,26 @@ class SnakeGame:
         pygame.display.flip()
 
     def buildSnake(self):
+        gridSize = 25
+        gridColor = pygame.Color("Black")
+        for x in range(0, window_width, gridSize):
+            pygame.draw.line(self.window, gridColor, (x, 0), (x, window_height))
+        for y in range(0, window_height, gridSize):
+            pygame.draw.line(self.window, gridColor, (0, y), (window_width, y))
+
+        
         self.window.blit(self.snake.head.block, (self.snake.head.x, self.snake.head.y))
+
+        self.window.blit(self.footer.block, (self.footer.x, self.footer.y))
+
+        score_text = pygame.font.SysFont(None, 30).render(f"Score: {self.score}", True, (0,0,0))
+        self.window.blit(score_text, (5, 500))
 
         self.window.blit(self.food.block.block, (self.food.block.x, self.food.block.y))
 
         for block in self.snake.body:
             self.window.blit(block.block, (block.x, block.y))
+        
 
 
 if __name__ == "__main__":
